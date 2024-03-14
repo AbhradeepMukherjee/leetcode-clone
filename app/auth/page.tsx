b473e8352b17e4438ceb57eeb02350945919fa40
Abhradeep Mukherjee <abhradeep22g@gmail.com>
2024-03-14 16:27:48 +0530

    problem table, home page and reset password features added

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,15 +1,28 @@
 "use client"
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from '@/components/Navbar/Navbar';
 import AuthModal from '@/components/Modals/AuthModal';
 import { useRecoilValue } from "recoil";
 import { authModalState } from '@/atoms/AuthModalAtom';
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
 type AuthPageProps = {
     
 };
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+	const [pageLoading, setPageLoading] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (user) router.push("/");
+		if (!loading && !user) setPageLoading(false);
+	}, [user, router, loading]);
+
+	if (pageLoading) return null;
     return (<div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
     <div className='max-w-7xl mx-auto'>
         <Navbar />
