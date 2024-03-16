4a6f25aa02dabc0910b7b921e6f0f8e3cd1489cf
Abhradeep Mukherjee <abhradeep22g@gmail.com>
2024-03-16 17:58:02 +0530

    getstaticpropserror

--- components/Workspace/Workspace.tsx
+++ components/Workspace/Workspace.tsx
@@ -3,15 +3,16 @@ import { useState } from "react";
 import Split from "react-split";
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
 import Playground from "./Playground/Playground";
+import { Problem } from "@/utils/types/problem";
 // import { Problem } from "@/utils/types/problem";
 // import Confetti from "react-confetti";
 // import useWindowSize from "@/hooks/useWindowSize";
 
 type WorkspaceProps = {
-    
+    problem: Problem
 };
 
-const Workspace:React.FC<WorkspaceProps> = () => {
+const Workspace:React.FC<WorkspaceProps> = ({problem}) => {
     // const { width, height } = useWindowSize();
 	const [success, setSuccess] = useState(false);
 	const [solved, setSolved] = useState(false);
